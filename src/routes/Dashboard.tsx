f32bfc02d4aef352fe48c713d1fc223b92a01dbc
uktveris <marius.uktveris@gmail.com>
2024-12-23 11:37:51 +0200

    edit to display files, not msg

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -3,14 +3,19 @@ import { useAxiosPrivate } from "../hooks/useAxiosPrivate";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
+interface FileMetadata {
+  name: string;
+  size: number;
+  fileType: string;
+  uploadedAt: Date;
+}
+
 function Dashboard() {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [loadingMsg, setLoadingMsg] = useState(true);
+  const [files, setFiles] = useState<FileMetadata[]>([]);
+  const [loadingFiles, setLoadingFiles] = useState(true);
   const { accessToken } = useAuth();
   const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
-  // test
-  const [inputValue, setInputValue] = useState("");
 
   const handleNavToHome = () => {
     navigate("/home");
@@ -20,35 +25,23 @@ function Dashboard() {
     navigate("/settings");
   };
 
-  const postNewMsg = async () => {
-    if (inputValue.trim() === "") {
-      console.log("msg cannot be empty..");
-      return;
-    }
-    try {
-      const response = await axiosPrivate.post("/files/post-message", {
-        inputValue,
-      });
-      getMessages();
-      console.log("dashboard: post message response: " + response.data);
-    } catch (err) {
-      console.log("ERROR: dashboard: " + (err as Error).message);
-    }
-  };
-
-  const handleInputChange = (event: any) => {
-    setInputValue(event.target.value);
-  };
-
   const getMessages = async () => {
     try {
       const response = await axiosPrivate.get("/files");
-      console.log("messages response:");
-      console.log(response.data);
-      setLoadingMsg(false);
-      setMessages(response.data.mappedMsg);
+      const filesArr: FileMetadata[] = [];
+      response.data.files.forEach((file) => {
+        filesArr.push({
+          name: file.name,
+          size: file.size,
+          fileType: file.fileType,
+          uploadedAt: file.uploadDateTime,
+        });
+      });
+      console.log(files);
+      setLoadingFiles(false);
+      setFiles(filesArr);
     } catch (err) {
-      setLoadingMsg(false);
+      setLoadingFiles(false);
       console.log("ERROR: dashboard: " + (err as Error).message);
     }
   };
@@ -60,21 +53,17 @@ function Dashboard() {
     <>
       <h1>This is the dashboard page</h1>
       <p> this is some sample text</p>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="message.."
-      />
-      <button onClick={postNewMsg}>add message</button>
       <h4>Messages: </h4>
-      {loadingMsg && <p>Loading...</p>}
-      {!loadingMsg && messages.length === 0 && (
-        <p>No messages available. or you dont have access to them. </p>
+      {loadingFiles && <p>Loading...</p>}
+      {!loadingFiles && files.length === 0 && (
+        <p>No files available. or you dont have access to them. </p>
       )}
-      {messages.map((message, index) => (
+      {files.map((file, index) => (
         <div key={index}>
-          <p>{message}</p>
+          <p>file: {file.name}</p>
+          <p>size: {file.size}</p>
+          <p>type: {file.fileType}</p>
+          <p>uploaded at: {file.uploadedAt.toString()}</p>
         </div>
       ))}
       <button onClick={handleNavToHome}>Home</button>
--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,49 +1,8 @@
-import { useState } from "react";
-import useAuth from "../hooks/useAuth";
-import { useAxiosPrivate } from "../hooks/useAxiosPrivate";
-
 function Settings() {
-  const { authed } = useAuth();
-  const axiosPrivate = useAxiosPrivate();
-  const [fileTest, setFileTest] = useState<{ title: string; desc: string }[]>(
-    [],
-  );
-  const [loading, setLoading] = useState(true);
-
-  const handleClick = async () => {
-    if (!authed) {
-      return;
-    }
-
-    try {
-      const response =
-        await axiosPrivate.get<{ title: string; desc: string }[]>(
-          "/files/files-test",
-        );
-      setLoading(false);
-      setFileTest(response.data);
-    } catch (err) {
-      setLoading(false);
-      console.log("ERROR: settings: " + (err as Error).message);
-    }
-  };
-
   return (
     <>
       <h1>The settings page here</h1>
       <p>Some settings to play aroud here</p>
-      <button onClick={handleClick}>click for fileTests</button>
-      <p>fileTests:</p>
-      {loading && <p>Loading fileTests..</p>}
-      {!loading && fileTest.length === 0 && (
-        <p>No fileTests are available, or you do not have access to them</p>
-      )}
-      {fileTest.map((f, index) => (
-        <div key={index}>
-          <h5>{f.title}</h5>
-          <p>{f.desc}</p>
-        </div>
-      ))}
     </>
   );
 }
